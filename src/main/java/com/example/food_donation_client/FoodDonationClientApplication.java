65406b634603bc013c64f8bd2446a919140ea191
Unnati Pulla <unnatipulla@unnatis-mbp-2.mynetworksettings.com>
2024-11-16 11:58:44 -0500

    Initial commit

---
+++ src/main/java/com/example/food_donation_client/FoodDonationClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.food_donation_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDonationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDonationClientApplication.class, args);
+	}
+
+}
