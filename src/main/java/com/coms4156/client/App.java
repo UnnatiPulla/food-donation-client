0f0097dbb7cce7134aa929cf2988e6d5c3409b63
UnnatiPulla <32179955+UnnatiPulla@users.noreply.github.com>
2024-11-21 16:24:08 -0500

    Get nearby listings - working. (#10)
    Co-authored-by: Unnati Pulla <unnatipulla@unnatis-mbp-2.mynetworksettings.com>

--- src/main/java/com/coms4156/client/App.java
+++ src/main/java/com/coms4156/client/App.java
@@ -1,37 +1,48 @@
 package com.coms4156.client;
 
-import com.google.gson.JsonObject;
-import org.apache.hc.core5.reactor.Command;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import java.io.IOException;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class App implements CommandLineRunner {
+    @Autowired
+    private RestTemplate restTemplate;
 
     public static void main(String[] args) { SpringApplication.run(App.class, args); }
 
     @Override
     public void run(String... args) throws IOException {
-        // UserAuthentication userAuth = new UserAuthentication();
-        // // Try to register a user
-        // System.out.println("Registering a new user...");
-        // JsonObject regResult = userAuth.registerUser("TEST_USER2", "TEST_PW");
-        // System.out.println("Result: " + regResult);
 
-        // // Try to authenticate a user
-        // System.out.println("Authenticating users...");
-        // // Should be ok
-        // JsonObject authResult = userAuth.authenticateUser("TEST_USER2", "TEST_PW");
-        // System.out.println("Result: " + authResult);
-        // // Should fail
-        // JsonObject authResult2 = userAuth.authenticateUser("TEST_USER2", "Talksjflka");
-        // System.out.println("Result: " + authResult2);
+        System.out.println("Fetching all food listings:");
+        MainController mainController = new MainController(restTemplate);
+        for (FoodListing f: mainController.getFoodListings()){
+            System.out.println(f.getFoodType());
+        }
+
+        System.out.println("Fetching nearby food listings in 1 mile radius:");
+        for (FoodListing f: mainController.getNearbyListings((float)40.7128, (float)-74.006, 1)){
+            System.out.println(f.getFoodType());
+        }
+
+
+//        UserAuthentication userAuth = new UserAuthentication();
+//        // Try to register a user
+//        System.out.println("Registering a new user...");
+//        JsonObject regResult = userAuth.registerUser("TEST_USER2", "TEST_PW");
+//        System.out.println("Result: " + regResult);
+//
+//        // Try to authenticate a user
+//        System.out.println("Authenticating users...");
+//        // Should be ok
+//        JsonObject authResult = userAuth.authenticateUser("TEST_USER2", "TEST_PW");
+//        System.out.println("Result: " + authResult);
+//        // Should fail
+//        JsonObject authResult2 = userAuth.authenticateUser("TEST_USER2", "Talksjflka");
+//        System.out.println("Result: " + authResult2);
     }
 
 }
--- src/main/java/com/coms4156/client/RouteController.java
+++ src/main/java/com/coms4156/client/RouteController.java
@@ -4,14 +4,12 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RouteController {
 
-    @Autowired private FoodListingService foodListingService;
+    @Autowired private MainController mainController;
 
     @GetMapping("/homescreen")
     public String homescreen() {
@@ -33,7 +31,7 @@ public class RouteController {
 
     @GetMapping("/food-request")
     public String foodRequest() {
-        List<FoodListing> listings = foodListingService.getFoodListings();
+        List<FoodListing> listings = mainController.getFoodListings();
         for (FoodListing l : listings) {
             System.out.println(l.getFoodType() + " " + l.getQuantityListed() + " " +
                                l.getEarliestPickUpTime() + " " + l.getLatitude() + " " +
