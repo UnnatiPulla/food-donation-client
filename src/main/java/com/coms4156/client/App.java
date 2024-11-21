572a6d52bf5f6213a533908bd9853b18ea122a35
lushaojia <71670653+lushaojia@users.noreply.github.com>
2024-11-21 18:35:42 -0500

    fix: add some working examples for UserAuth w/o Firebase Admin SDK

--- src/main/java/com/coms4156/client/App.java
+++ src/main/java/com/coms4156/client/App.java
@@ -1,10 +1,12 @@
 package com.coms4156.client;
 
+import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.IOException;
+import java.util.UUID;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -28,21 +30,34 @@ public class App implements CommandLineRunner {
             System.out.println(f.getFoodType());
         }
 
+        UserAuthentication userAuth = new UserAuthentication();
+        // Try to register a user
+        System.out.println("Registering a new user...");
+        // Should fail: user already exists
+        JsonObject regResult = userAuth.registerUser("TEST_USER2", "TEST_PW");
+        System.out.println("Result: " + regResult);
+        // Should be ok
+        JsonObject regResult2 = userAuth.registerUser(UUID.randomUUID().toString(), "maowewnuno");
+        System.out.println("Result: " + regResult2);
+        // Should fail: username cannot be empty
+        JsonObject regResult3 = userAuth.registerUser("", "!!!!!");
+        System.out.println("Result: " + regResult3);
 
-//        UserAuthentication userAuth = new UserAuthentication();
-//        // Try to register a user
-//        System.out.println("Registering a new user...");
-//        JsonObject regResult = userAuth.registerUser("TEST_USER2", "TEST_PW");
-//        System.out.println("Result: " + regResult);
-//
-//        // Try to authenticate a user
-//        System.out.println("Authenticating users...");
-//        // Should be ok
-//        JsonObject authResult = userAuth.authenticateUser("TEST_USER2", "TEST_PW");
-//        System.out.println("Result: " + authResult);
-//        // Should fail
-//        JsonObject authResult2 = userAuth.authenticateUser("TEST_USER2", "Talksjflka");
-//        System.out.println("Result: " + authResult2);
+
+        // Try to authenticate a user
+        System.out.println("Authenticating users...");
+        // Should be ok
+        JsonObject authResult = userAuth.authenticateUser("TEST_USER2", "TEST_PW");
+        System.out.println("Result: " + authResult);
+        // Should fail: user doesn't exist
+        JsonObject authResult2 = userAuth.authenticateUser("wrong-user!", "blah");
+        System.out.println("Result: " + authResult2);
+        // Should fail: wrong pass word
+        JsonObject authResult3 = userAuth.authenticateUser("TEST_USER2", "Talksjflka");
+        System.out.println("Result: " + authResult3);
+        // Authenticating user that's already authenticated: ok!
+        JsonObject authResult4 = userAuth.authenticateUser("TEST_USER2", "TEST_PW");
+        System.out.println("Result: " + authResult4);
     }
 
 }
