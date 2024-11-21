1ce3abedb231aa6e5d86917bc4b895dcb8a6ee6f
lushaojia <71670653+lushaojia@users.noreply.github.com>
2024-11-21 12:46:06 -0500

    feat: implement user registration and authentication using Firebase

--- src/main/java/com/example/food_donation_client/FoodDonationClientApplication.java
+++ src/main/java/com/example/food_donation_client/FoodDonationClientApplication.java
@@ -1,9 +1,11 @@
 package com.example.food_donation_client;
 
+import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
 
 @SpringBootApplication
 public class FoodDonationClientApplication implements CommandLineRunner {
@@ -16,9 +18,24 @@ public class FoodDonationClientApplication implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String... args) {
-		// Call the method in FoodListingService
-		System.out.println("Fetching all available food listings...");
-		foodListingService.getAllAvailableFoodListings().forEach(System.out::println);
+	public void run(String... args) throws IOException {
+//		// Call the method in FoodListingService
+//		System.out.println("Fetching all available food listings...");
+//		foodListingService.getAllAvailableFoodListings().forEach(System.out::println);
+
+		// Try to register a user
+		System.out.println("Registering a new user...");
+		UserAuthentication userAuth = new UserAuthentication();
+		JsonObject regResult = userAuth.registerUser("TEST_USER2", "TEST_PW");
+		System.out.println("Result: " + regResult);
+
+		// Try to authenticate a user
+		System.out.println("Authenticating users...");
+		// Should be ok
+		JsonObject authResult = userAuth.authenticateUser("TEST_USER2", "TEST_PW");
+		System.out.println("Result: " + authResult);
+		// Should fail
+		JsonObject authResult2 = userAuth.authenticateUser("TEST_USER2", "Talksjflka");
+		System.out.println("Result: " + authResult2);
 	}
 }
