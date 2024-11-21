584d1a3b22604068b6d486adde69fc04ac4fe042
lushaojia <71670653+lushaojia@users.noreply.github.com>
2024-11-21 18:36:07 -0500

    patch: change variable names to get symbols to resolve

--- src/main/java/com/coms4156/client/RouteController.java
+++ src/main/java/com/coms4156/client/RouteController.java
@@ -1,10 +1,11 @@
 package com.coms4156.client;
 
-import java.time.format.DateTimeFormatter;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Controller
 public class RouteController {
@@ -15,18 +16,18 @@ public class RouteController {
     public String homescreen() {
         return "homescreen";
     }
-	
+
     @GetMapping("/searchresults")
     public String searchResults(Model model) {
-      List<FoodListing> listings = foodListingService.getFoodListings();
+      List<FoodListing> listings = mainController.getFoodListings();
 	  DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
       listings.forEach(listing -> {
         if (listing.getEarliestPickUpTime() != null) {
             listing.setFormattedPickUpTime(listing.getEarliestPickUpTime().format(formatter));
         }
      });
-      model.addAttribute("foodListings", listings); 
-      return "searchresults"; 
+      model.addAttribute("foodListings", listings);
+      return "searchresults";
     }
 
     @GetMapping("/food-request")
